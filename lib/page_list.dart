4294fc06e7531847cbb7e0c14d2b892b75a7c10d
Justin K -Linux <justinjoseph0007@gmail.com>
2019-11-12 10:37:16 -0500

    Added a list page

---
+++ lib/page_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'maps_list_page.dart';
+
+class PageList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Byke',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Byke'),
+        ),
+        body: ListView(
+          padding: EdgeInsets.all(8),
+          children: <Widget>[
+            RaisedButton(
+              child: Text("maps_list_page"),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  new MaterialPageRoute(
+                    builder: (context) => new MapsListPage(
+                      title: "Test title",
+                    ),
+                  ),
+                );
+              },
+            ),
+          ], // List of buttons
+        ),
+      ),
+    );
+  }
+}
