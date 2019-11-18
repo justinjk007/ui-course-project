f4c6e06a5a8f0f01be38838d46dd0dcf098b5f39
Justin K -Linux <justinjoseph0007@gmail.com>
2019-11-18 12:39:21 -0500

    All map cards work now

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,7 @@
 import 'package:byke/maps_list_page.dart';
 import 'package:byke/others_and_me.dart';
 import 'package:flutter/material.dart';
-
+import 'map_and_start_button.dart';
 import 'maps_list_widget.dart';
 
 class HomePage extends StatefulWidget {
@@ -13,89 +13,133 @@ class HomePage extends StatefulWidget {
 }
 
 class HomePageState extends State<HomePage> {
+  Widget makeContent(String main, String stars) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(main),
+        Text(
+          stars,
+          style: TextStyle(color: Colors.yellow[800]),
+          textScaleFactor: 1.5,
+        ),
+      ],
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     var _curIndex = 1;
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("Byke", textAlign: TextAlign.center)
-      ),
+      appBar: new AppBar(title: new Text("Byke", textAlign: TextAlign.center)),
       body: new Container(
-        child:ListView(
-          children: <Widget>[
-            CustomCard(), 
-            Padding(padding: EdgeInsets.all(5),  child: Text("Recently Completed:",  style: TextStyle(
+          child: ListView(
+        children: <Widget>[
+          CustomCard(),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Text(
+              "Recently Completed:",
+              style: TextStyle(
                   color: Colors.grey[800],
                   fontWeight: FontWeight.w900,
                   fontFamily: 'Open Sans',
                   fontSize: 20),
-            ),),
-            MapsListWidget(
-              title: "Don Valley Trail",
-              content: Text("Personal Time: 15 minutes\nRanking: 5th"),
-              height: 100,
-              image: "assets/map3_crop.jpg",
             ),
-            Padding(padding: EdgeInsets.all(5),  child: Text("Popular Nearby:",  style: TextStyle(
+          ),
+          MapsListWidget(
+            title: "Don Valley Trail",
+            content: makeContent(
+              "\nDistance: 500 meters\nCurrent record: 6 mins\nPoints Earned: 125\nDifficulty: hard\n",
+              "★★★✩✩",
+            ),
+            height: 170,
+            image: "assets/map3_crop.jpg",
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (context) => new MapAndStartButton(
+                    image: 'assets/map3.jpg',
+                  ),
+                ),
+              );
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.all(5),
+            child: Text(
+              "Popular Nearby:",
+              style: TextStyle(
                   color: Colors.grey[800],
                   fontWeight: FontWeight.w900,
                   fontFamily: 'Open Sans',
                   fontSize: 20),
-            ),),
-            MapsListWidget(
-              title: "Ontario Tech Loop",
-              content: Text("Personal Time: N/A minutes\nRanking: N/A"),
-              height: 100,
-              image: "assets/map1_crop.jpg",
             ),
-          ],)
-        ),
-        bottomNavigationBar: BottomNavigationBar(// this will be set when a new tab is tapped
-        items: [
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.view_list),
-            title: new Text('Trails'),
           ),
-          BottomNavigationBarItem(
-            icon: new Icon(Icons.home),
-            title: new Text('Home'),
+          MapsListWidget(
+            title: "Ontario Tech Loop",
+            height: 170,
+            content: makeContent(
+              "\nDistance: 700 meters\nCurrent record: 5 mins\nPoints Earned: 100\nDifficulty: medium\n",
+              "★★★★✩",
+            ),
+            image: "assets/map1_crop.jpg",
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (context) => new MapAndStartButton(
+                    image: 'assets/map1.jpg',
+                  ),
+                ),
+              );
+            },
           ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.search),
-            title: Text('Explore')
-          )
         ],
-        currentIndex: _curIndex,
-        onTap: (index) {
-          setState(() {
-            _curIndex = index;
-            switch (_curIndex) {
-              case 0:
-              Navigator.pushReplacement(
-                  context,
-                  new MaterialPageRoute(
-                    settings: RouteSettings(isInitialRoute: true),
-                    builder: (context) => new MapsListPage(),
-                  ),
-                );
-                break;
-              case 1:
-                break;
-              case 2:
-                Navigator.pushReplacement(
-                  context,
-                  new MaterialPageRoute(
-                    settings: RouteSettings(isInitialRoute: true),
-                    builder: (context) => new OthersAndMe(),
-                  ),
-                );
-            }
-          });
-        }
-
-        ),
-      );
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+          // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.view_list),
+              title: new Text('Trails'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text('Home'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text('Explore'))
+          ],
+          currentIndex: _curIndex,
+          onTap: (index) {
+            setState(() {
+              _curIndex = index;
+              switch (_curIndex) {
+                case 0:
+                  Navigator.pushReplacement(
+                    context,
+                    new MaterialPageRoute(
+                      settings: RouteSettings(isInitialRoute: true),
+                      builder: (context) => new MapsListPage(),
+                    ),
+                  );
+                  break;
+                case 1:
+                  break;
+                case 2:
+                  Navigator.pushReplacement(
+                    context,
+                    new MaterialPageRoute(
+                      settings: RouteSettings(isInitialRoute: true),
+                      builder: (context) => new OthersAndMe(),
+                    ),
+                  );
+              }
+            });
+          }),
+    );
   }
 }
 
@@ -103,32 +147,35 @@ class CustomCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-        child: new Stack(
-          children: <Widget>[
-            Card(
-              child: Container(
-                child: Center(
-                  child: ListView(
-                    padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
-                    children: <Widget>[
-                      new CircleAvatar(
-                        radius: 25.0,
-                        child: Text("A"),
-                      ),
-                      Center(
-                        child: Padding(child: Center( child: Text("Biking till I die!\nFriends: 50", textAlign: TextAlign.center,)), padding: EdgeInsets.all(10),)),
-                    ],
-                  ),
+      child: new Stack(
+        children: <Widget>[
+          Card(
+            child: Container(
+              child: Center(
+                child: ListView(
+                  padding: EdgeInsets.fromLTRB(0, 10, 0, 0),
+                  children: <Widget>[
+                    new CircleAvatar(
+                      radius: 25.0,
+                      child: Text("A"),
+                    ),
+                    Center(
+                        child: Padding(
+                      child: Center(
+                          child: Text(
+                        "Biking till I die!\nFriends: 50",
+                        textAlign: TextAlign.center,
+                      )),
+                      padding: EdgeInsets.all(10),
+                    )),
+                  ],
                 ),
-                height: 120.0,
               ),
+              height: 120.0,
             ),
-
-            
-          ],
-        ),
-      );
-      
-    
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/maps_list_page.dart
+++ lib/maps_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:byke/home_page.dart';
 import 'package:byke/others_and_me.dart';
 import 'package:flutter/material.dart';
+import 'map_and_start_button.dart';
 import 'maps_list_widget.dart';
 
 class MapsListPage extends StatefulWidget {
@@ -13,7 +14,6 @@ class MapsListPage extends StatefulWidget {
 
 class _MapsListPageState extends State<MapsListPage> {
   // TODO: Add hero animation from this page
-  // TODO: use https://pub.dev/packages/expandable_card
   Widget makeContent(String main, String stars) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
@@ -31,90 +31,113 @@ class _MapsListPageState extends State<MapsListPage> {
   @override
   Widget build(BuildContext context) {
     var _curIndex = 0;
-        return Scaffold(
-          appBar: AppBar(
-            title: Text("Maps list page"),
-          ),
-          body: ListView(
-            padding: EdgeInsets.all(8),
-            children: <Widget>[
-              MapsListWidget(
-                title: "OnTechU circle",
-                content: makeContent(
-                  "\nDistance: 700 meters\nCurrent record: 5 mins\nPoints Earned: 100\nDifficulty: medium\n",
-                  "★★★★✩",
-                ),
-                height:170,
-                image: "assets/map1_crop.jpg",
-                // onTap:
-              ),
-              MapsListWidget(
-                title: "7 Stretch",
-                height: 170,
-                content: makeContent(
-                  "\nDistance: 730 meters\nCurrent record: 3.5 mins\nPoints Earned: 80\nDifficulty: easy\n",
-                  "★★★★★",
-                ),
-                image: "assets/map2_crop.jpg",
-                // onTap:
-              ),
-              MapsListWidget(
-                title: "That road",
-                height: 170,
-                content: makeContent(
-                  "\nDistance: 500 meters\nCurrent record: 6 mins\nPoints Earned: 125\nDifficulty: hard\n",
-                  "★★★✩✩",
-                ),
-                image: "assets/map3_crop.jpg",
-                // onTap:
-              ),
-            ], // List
-          ),
-                  bottomNavigationBar: BottomNavigationBar(// this will be set when a new tab is tapped
-            items: [
-              BottomNavigationBarItem(
-                icon: new Icon(Icons.view_list),
-                title: new Text('Trails'),
-              ),
-              BottomNavigationBarItem(
-                icon: new Icon(Icons.home),
-                title: new Text('Home'),
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                title: Text('Explore')
-              )
-            ],
-            currentIndex: _curIndex = 0,
-        onTap: (index) {
-          setState(() {
-            _curIndex = index;
-            switch (_curIndex) {
-              case 0:
-              
-                break;
-              case 1:
-              Navigator.pushReplacement(
-                  context,
-                  new MaterialPageRoute(
-                    settings: RouteSettings(isInitialRoute: true),
-                    builder: (context) => new HomePage(),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Maps list page"),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(8),
+        children: <Widget>[
+          MapsListWidget(
+            title: "OnTechU circle",
+            content: makeContent(
+              "\nDistance: 700 meters\nCurrent record: 5 mins\nPoints Earned: 100\nDifficulty: medium\n",
+              "★★★★✩",
+            ),
+            height: 170,
+            image: "assets/map1_crop.jpg",
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (context) => new MapAndStartButton(
+                    image: 'assets/map1.jpg',
                   ),
-                );
-                break;
-              case 2:
-                Navigator.pushReplacement(
-                  context,
-                  new MaterialPageRoute(
-                    settings: RouteSettings(isInitialRoute: true),
-                    builder: (context) => new OthersAndMe(),
+                ),
+              );
+            },
+          ),
+          MapsListWidget(
+            title: "7 Stretch",
+            height: 170,
+            content: makeContent(
+              "\nDistance: 730 meters\nCurrent record: 3.5 mins\nPoints Earned: 80\nDifficulty: easy\n",
+              "★★★★★",
+            ),
+            image: "assets/map2_crop.jpg",
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (context) => new MapAndStartButton(
+                    image: 'assets/map2.jpg',
                   ),
-                );
-            }
-          });
-        }
-
-        ),
+                ),
+              );
+            },
+          ),
+          MapsListWidget(
+            title: "That road",
+            height: 170,
+            content: makeContent(
+              "\nDistance: 500 meters\nCurrent record: 6 mins\nPoints Earned: 125\nDifficulty: hard\n",
+              "★★★✩✩",
+            ),
+            image: "assets/map3_crop.jpg",
+            onTap: () {
+              Navigator.push(
+                context,
+                new MaterialPageRoute(
+                  builder: (context) => new MapAndStartButton(
+                    image: 'assets/map3.jpg',
+                  ),
+                ),
+              );
+            },
+          ),
+        ], // List
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+          // this will be set when a new tab is tapped
+          items: [
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.view_list),
+              title: new Text('Trails'),
+            ),
+            BottomNavigationBarItem(
+              icon: new Icon(Icons.home),
+              title: new Text('Home'),
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.search), title: Text('Explore'))
+          ],
+          currentIndex: _curIndex = 0,
+          onTap: (index) {
+            setState(() {
+              _curIndex = index;
+              switch (_curIndex) {
+                case 0:
+                  break;
+                case 1:
+                  Navigator.pushReplacement(
+                    context,
+                    new MaterialPageRoute(
+                      settings: RouteSettings(isInitialRoute: true),
+                      builder: (context) => new HomePage(),
+                    ),
+                  );
+                  break;
+                case 2:
+                  Navigator.pushReplacement(
+                    context,
+                    new MaterialPageRoute(
+                      settings: RouteSettings(isInitialRoute: true),
+                      builder: (context) => new OthersAndMe(),
+                    ),
+                  );
+              }
+            });
+          }),
     );
   }
 }
